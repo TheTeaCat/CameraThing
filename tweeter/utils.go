39b99dd46f9b908a43c728fd649d0ff1fdebe13d
TheTeaCat <joshua.r.osullivan@gmail.com>
2021-05-08 16:56:06 +0100

    Docs

--- tweeter/utils.go
+++ tweeter/utils.go
@@ -1,10 +1,12 @@
 package main
 
-//Response from the recogniser service
+//The format of a response from the recogniser service
 type ClassifyResult struct {
 	Filename string        `json:"filename"`
 	Labels   []LabelResult `json:"labels"`
 }
+
+//A label from within a ClassifyResult as returned by the recogniser service
 type LabelResult struct {
 	Label       string  `json:"label"`
 	Probability float32 `json:"probability"`
