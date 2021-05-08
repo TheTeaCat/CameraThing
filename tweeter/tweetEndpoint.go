5e9cb157056466fd8fe48c8c68f837ca138c3ac6
TheTeaCat <joshua.r.osullivan@gmail.com>
2021-05-08 15:37:00 +0100

    Add image title to tweet endpoint response

--- tweeter/tweetEndpoint.go
+++ tweeter/tweetEndpoint.go
@@ -64,5 +64,7 @@ func (te *tweetEndpoint) handle(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode("Successfully made tweet")
+	json.NewEncoder(w).Encode(map[string]string{
+		"Title": imageTitle,
+	})
 }
