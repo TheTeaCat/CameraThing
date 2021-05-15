dd7fe68e2c52614c32cec6617980319390947a58
TheTeaCat <joshua.r.osullivan@gmail.com>
2021-05-15 16:12:03 +0100

    Don't use GPS, not enough GPIO :(

--- camera-thing/main/main.cpp
+++ camera-thing/main/main.cpp
@@ -5,9 +5,12 @@
 #include "main.h"
 #include "utils.h"
 #include "camera.h"
-#include "geolocate.h"
 #include "tweeter.h"
 #include "asyncLed.h"
+
+//I would like to use the GPS featherwing but I have actually just ran out of 
+//GPIO pins...
+//#include "geolocate.h"
 
 /////////////////////////////////////////////////////////////////////////////
 // Debugging flags
@@ -77,16 +80,16 @@ void setup() {
   Serial.println("Tweeter is accessible!");
 
   //Setup GPS. Should be pretty fast...
-  Serial.println("Setting up GPS...");
-  bool gpsSuccess = setupGPS();
-  if (!gpsSuccess) {
-    Serial.println("Failed to setup GPS :(");
-    //Signal hardware failure
-    myLed.blink(100);
-    WAIT_MS(2000);
-    ESP.restart();
-  }
-  Serial.println("Set up GPS!");
+  // Serial.println("Setting up GPS...");
+  // bool gpsSuccess = setupGPS();
+  // if (!gpsSuccess) {
+  //   Serial.println("Failed to setup GPS :(");
+  //   //Signal hardware failure
+  //   myLed.blink(100);
+  //   WAIT_MS(2000);
+  //   ESP.restart();
+  // }
+  // Serial.println("Set up GPS!");
 
   //Setup camera. This may take a while if something has gone wrong...
   Serial.println("Setting up camera...");
@@ -168,52 +171,53 @@ void loop() {
     //This step is optional and only completed if the user doesn't press down 
     //the button after a second
     float lat; float lon;
+    bool geolocationEnabled = false;
 
-    //Give the user 2.4 seconds to hold the button down to disable geolocation
-    Serial.println("[Loop] - Getting geolocation preference from user...");
+    // //Give the user 2.4 seconds to hold the button down to disable geolocation
+    // Serial.println("[Loop] - Getting geolocation preference from user...");
 
-    //Communicate input request with fast breathe
-    myLed.breathe(300);
-    WAIT_MS(2400);
+    // //Communicate input request with fast breathe
+    // myLed.breathe(300);
+    // WAIT_MS(2400);
 
-    //If the user has released the button after 2 seconds, geodata isn't used.
-    bool geolocationEnabled = digitalRead(buttonPin) == LOW;
-    if(!geolocationEnabled) {
-      Serial.println("[Loop] - Geolocation disabled.");
-    } else {
-      Serial.println("[Loop] - Geolocation enabled.");
-    }
+    // //If the user has released the button after 2 seconds, geodata isn't used.
+    // bool geolocationEnabled = digitalRead(buttonPin) == LOW;
+    // if(!geolocationEnabled) {
+    //   Serial.println("[Loop] - Geolocation disabled.");
+    // } else {
+    //   Serial.println("[Loop] - Geolocation enabled.");
+    // }
 
-    if (geolocationEnabled) {
-      //Communicate geolocating with a fast breathe
-      myLed.throb(900,100);
+    // if (geolocationEnabled) {
+    //   //Communicate geolocating with a fast breathe
+    //   myLed.throb(900,100);
 
-      //Get geolocation with geolocate() function
-      bool gotGeolocation = geolocate(&lat, &lon, 1000);
+    //   //Get geolocation with geolocate() function
+    //   bool gotGeolocation = geolocate(&lat, &lon, 1000);
 
-      //If we're mocking delays, delay to pretend it takes a while to get a 
-      //geolocation from the GPS featherwing
-      #ifdef MOCKDELAY
-        Serial.printf("[MockDelay] - Mocking a slower GPS by waiting %d ms...\n", MOCKDELAY);
-        WAIT_MS(MOCKDELAY);
-      #endif
+    //   //If we're mocking delays, delay to pretend it takes a while to get a 
+    //   //geolocation from the GPS featherwing
+    //   #ifdef MOCKDELAY
+    //     Serial.printf("[MockDelay] - Mocking a slower GPS by waiting %d ms...\n", MOCKDELAY);
+    //     WAIT_MS(MOCKDELAY);
+    //   #endif
 
-      //If there is some err getting the geolocation data, signal an err and 
-      //return.
-      if(!gotGeolocation) {
-        myLed.blink(100); //blink(100) for hardware failure
-        WAIT_MS(2000);
-        myLed.off();
-        delete jpgBuffer;
-        return;
-      }
+    //   //If there is some err getting the geolocation data, signal an err and 
+    //   //return.
+    //   if(!gotGeolocation) {
+    //     myLed.blink(100); //blink(100) for hardware failure
+    //     WAIT_MS(2000);
+    //     myLed.off();
+    //     delete jpgBuffer;
+    //     return;
+    //   }
 
-      //Output geolocation to serial
-      Serial.printf("[Loop] - Got geolocation, lat: %f, long: %f\n", lat, lon);
+    //   //Output geolocation to serial
+    //   Serial.printf("[Loop] - Got geolocation, lat: %f, long: %f\n", lat, lon);
 
-      //Turn the LED off now the GPS is done
-      myLed.off();
-    }
+    //   //Turn the LED off now the GPS is done
+    //   myLed.off();
+    // }
 
     //////////////////////////////////////////////////////////////////////
     //Upload
