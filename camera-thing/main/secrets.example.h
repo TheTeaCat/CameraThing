e536e4a438100480b07caad7f9585d7c934a23e9
TheTeaCat <joshua.r.osullivan@gmail.com>
2021-05-15 12:47:28 +0100

    Doc secrets.example.h

--- camera-thing/main/secrets.example.h
+++ camera-thing/main/secrets.example.h
@@ -1,11 +1,20 @@
-//Credentials for using the wifiClient in wifiClient.cpp
+///////////////////////////////////////////////////////////////////////////
+// Network credentials
+// Note: Either WiFi OR 2G must be used; not both.
+
+//Credentials for using the webClient in wifiClient.h
+//If WIFI_SSID is defined then the CameraThing will attempt to use WiFi.
 #define WIFI_SSID "Put your SSID in here! :)"
 #define WIFI_PASS "Put your password in here! :)"
 
-//GPRS credentials
+//GPRS credentials for the webClient in gprsClient.h
+//If APN is defined then the CameraThing will attempt to use 2G
 #define APN "Put the APN of your provider here :)"
 #define GPRS_USER "Put the username for your APN here :)"
 #define GPRS_PASS "Put the password for your APN here :)"
+
+///////////////////////////////////////////////////////////////////////////
+// Cloud services
 
 //Details for your tweeter service
 #define TWEETER_HOST "Put the hostname of your tweeter service here! :)"
